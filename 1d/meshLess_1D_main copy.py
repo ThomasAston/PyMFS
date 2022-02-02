b74ffc716c8512222312ef362217a35cd3022493
Your Name <tjn.aston@gmail.com>
2022-02-02 12:46:19 +0000

    adding 1D

--- 1d/meshLess_1D_main copy.py
+++ 1d/meshLess_1D_main copy.py
@@ -1,9 +1,9 @@
 # meshLess_1D v1
 
 # Author(s): Thomas Aston
-# Date updated: 27/01/2022
+# Date updated: 02/02/2022
 
-# Main file for calling functions for solving 1D problems using MFS
+# Main file for solving 1D axial bar problem using MFS
 
 # To do list:
 # 
@@ -11,77 +11,173 @@
 #
 
 import numpy as np
-
 import sympy as sym
-from sympy import *
-from sympy.plotting import plot
-
 from matplotlib import pyplot as plt
-init_printing( use_latex='mathjax' )  # use pretty math output
-# Set up the problem...
 
+##############################################
+# PROBLEM DEFINITION
+##############################################
+
+# We want to solve Poisson's equation for a 1D bar subjected to distributed axial
+# loading. Analytical displacement field is chosen such that the following domain
+# properties and boundary values are valid.
+
+### Domain ###
 l = 1  # Domain length
+
+### Discretisation ###
 N = 6   # Number of nodes 
 r = 0.2 # Sphere radius
+x = np.linspace(0, l, N) # Generate nodes
+print('List of nodal coordinates: ',x)
+order = 2 # Number of terms in the local basis (= number of required shape functions)
 
-X = np.linspace(0, l, N) # Generate nodes
-
-print('List of nodal coordinates: ',X)
-
+### BCs ###
 u_s = 1 # Essential boundary at x = 0
-
 f_s = 2 # Natural boundary at x = 1
 
-# Analytical solution...
-x = sym.Symbol('x')
+### Analytical displacement field ###
+resolution = 100
+dx = l/resolution
+X = np.linspace(0,1,resolution)
+u_analytical = []
+for i in range(0,len(X)):
+    u_analytical.append(0.5*(X[i]-(X[i]**3)/3)+2*X[i]+1)
 
-u_analytical = 0.5*(x-x**3/3)+2*x+1
 
-# print(u_analytical)
-p1 = plot(u_analytical,xlim=(0,1), ylim=(0,3.5), show=False)
+##############################################
+# GENERATE SHAPE FUNCTIONS
+##############################################
 
-# Generate the shape functions...
-phi = []
-x_i = 0
+### Initialise empty arrays ###
+phi = np.zeros((len(x),resolution)) # Shepard PU functions
+W_i = np.zeros((len(x),resolution)) # Weight functions
+h = np.zeros((len(x),order,resolution)) # Shape functions
 
-W_i = []
-sum_W_j = []
-phi = []
-h = []
+### Generate by looping over nodes ###
+for i in range(N): 
+    for a in range(len(X)):         
+        # Calculate the normalised distance from node i
+        s = np.abs(X[a] - x[i])/r
+
+        # Calculate W_i
+        if s <= 1:
+            W_i[i][a] = 1-6*s**2+8*s**3-3*s**4
+        else:
+            W_i[i][a] = 0
+
+        # Calculate W_j for each of the other nodes
+        sum_W_j =0
+        for j in range(N):
+            s_j = np.abs(X[a] - x[j])/r
+            
+            if s_j<=1:
+                W_j = 1-6*s_j**2+8*s_j**3-3*s_j**4
+            else:
+                W_j = 0
+
+            sum_W_j += W_j
+
+        # Calculate the Shepard PU function
+        phi[i][a] = W_i[i][a]/sum_W_j
+        
+        # Compute the 2 shape functions
+        if order == 2:
+            h[i][0][a] = phi[i][a]
+            h[i][1][a] = phi[i][a] * (X[a] - x[i])/r
+
+# Differentiate the shape functions
+dh_dx = np.zeros((len(x),order,resolution))
 for i in range(N):
-    s_i = ((x - x_i)**2)**(0.5)/r
+    for j in range(order):
+        for a in range(resolution-1):
+            dh_dx[i][j][a] = (h[i][j][a+1] - h[i][j][a])/(X[a+1]-X[a])
 
-    W_i.append(sym.Piecewise((1 - 6*s_i**2 + 8*s_i**3 - 3*s_i**4, s_i<=1) , (0, s_i>1)))
+
+##############################################
+# ASSEMBLE STIFFNESS MATRIX
+##############################################
+
+### Initialise empty arrays ###
+K = np.zeros((N,N,order,order)) # Stiffness matrix
+K_open = np.zeros((N*order,N*order)) # Unravelled stiffness matrix
+
+### Loop over nodes and DoFs ###
+for i in range(N):
+    for j in range(N):
+        for m in range(order):
+            for n in range(order):    
+                
+                # Loop from x1 to x2 to numerically integrate...
+                for a in range(0, resolution):
+                    if a == 0 or a==resolution:#(resolution/(N-1)):
+                        f_x = dh_dx[i][m][a] * dh_dx[j][n][a]
+                    else:
+                        f_x = 2*dh_dx[i][m][a] * dh_dx[j][n][a]
+
+                    K[i][j][m][n] += dx/2*f_x
+                    K_open[order*i:order+order*i,order*j:order+order*j] = K[i,j,:,:]
+                    
     
-    x_j = 0
-    W_j = []
-    for j in range(N):        
-        s_j = ((x - x_j)**2)**(0.5)/r
-        W_j.append(sym.Piecewise((1 - 6*s_j**2 + 8*s_j**3 - 3*s_j**4, s_j<=1) , (0, s_j>1)))
-        x_j+=1/(N-1)
-    
-    sum_W_j.append(np.sum(W_j))
-    
-    phi.append(W_i[i]/sum_W_j[i])
+##############################################
+# IMPLEMENT BOUNDARY CONDITIONS
+##############################################
 
-    h.append(phi[i])
-    h.append(phi[i]*(x-x_i)/r)
+f_Im = np.zeros((N, order))
+fHat_Im = np.zeros((N,order))
+KU_ImJn = np.zeros((N,N,order,order))
+KU_ImJn_open = np.zeros((N*order,N*order))
 
-    x_i+=l/(N-1)
+for i in range(N):
+    for m in range(order):
+        
+        f_Im[i][m] = np.trapz(X[:]*h[i][m][:], x=None, dx=dx)
 
-h = np.reshape(h, (N, 2))
+        # Evaluate fHat_Im 
+        if i==5: # node is on neumann boundary
+            fHat_Im[i][m] = f_s * h[i][m][resolution-1]
+        elif i==0: # node is on dirichlet boundary
+            
+            fU_Im = u_s*dh_dx[i][m][0]
+            fHat_Im[i][m] =  fU_Im
 
-# p2 = plot(h[1,0],h[1,1],xlim=(0,1),show=True)
+            for j in range(N):
+                for n in range(order):
+                    KU_ImJn[i][j][m][n] = -((h[i][m][1]*h[j][n][1] - h[i][m][0]*h[j][n][0]))/(X[1]-X[0])
+                    KU_ImJn_open[order*i:order+order*i,order*j:order+order*j] = K[i,j,:,:]
+        else: 
+            fHat_Im[i][m] = 0
 
-# Assemble the stiffness matrix...
 
-# k = np.zeros([2,2])
-K = np.zeros([N*2, N*2])
-# print(K)
+##############################################
+# SOLVE SYSTEM OF EQUATIONS
+##############################################
+K_open = K_open-KU_ImJn_open
+f = f_Im + fHat_Im
+f = np.ravel(f)
 
-print(diff(h[0,0],x))
-K[0,0] = integrate(diff(h[0,0],x)*diff(h[0,0],x),(x,0,1))
+K_ff = K_open[order:,order:]
+f_f = f[order:]
+K_fr = K_open[order:,0:order]
+if order == 2:
+    q_r = [u_s, 0]
 
-print(K)
+F = f_f-(K_fr @ q_r)
+q_f = np.linalg.solve(K_ff,F)
+print(q_f)
 
-# plt.show()
+u_f = q_f[0::order]
+u = np.insert(u_f,0,u_s)
+
+
+##############################################
+# PLOT RESULTS
+##############################################
+ax2 = plt.axes
+plt.plot(X,u_analytical)
+plt.scatter(x,u)
+
+plt.xlabel("x")
+plt.ylabel("u(x)")
+
+plt.show()
--- 1d/meshLess_1D_main.py
+++ 1d/meshLess_1D_main.py
@@ -29,7 +29,7 @@ u_s = 1 # Essential boundary at x = 0
 f_s = 2 # Natural boundary at x = 1
 
 # Analytical solution...
-resolution = 100
+resolution = 1000
 dx = l/resolution
 X = np.linspace(0,1,resolution)
 
@@ -37,15 +37,13 @@ u_analytical = []
 for i in range(0,len(X)):
     u_analytical.append(0.5*(X[i]-(X[i]**3)/3)+2*X[i]+1)
 
-
 # Generate shape functions at each node...
-
 # Start by initialising empty phi array, which includes rows for each node
-phi = np.zeros([len(x),resolution])
-W_i = np.zeros([len(x),resolution])
-W_j = np.zeros([len(x),resolution])
-h = np.zeros([len(x),2,resolution])
+phi = np.zeros((len(x),resolution))
+W_i = np.zeros((len(x),resolution))
+h = np.zeros((len(x),2,resolution))
 
+# print(W_i[5][100])
 for i in range(N):
     print('Current node: ', i+1)
     for a in range(len(X)):         
@@ -54,48 +52,108 @@ for i in range(N):
 
         # Calculate W_i
         if s <= 1:
-            W_i[i,a] = 1-6*s**2+8*s**3-3*s**4
+            W_i[i][a] = 1-6*s**2+8*s**3-3*s**4
         else:
-            W_i[i,a] = 0
+            W_i[i][a] = 0
 
         # Calculate W_j for each of the other nodes
-        for j in range(0,N):
+        sum_W_j =0
+        for j in range(N):
             s_j = np.abs(X[a] - x[j])/r
             
             if s_j<=1:
-                W_j[j,a] = 1-6*s_j**2+8*s_j**3-3*s_j**4
+                W_j = 1-6*s_j**2+8*s_j**3-3*s_j**4
             else:
-                W_j[j,a] = 0
+                W_j = 0
 
-        phi[i,a] = W_i[i,a]/sum(W_j[:,a])
+            sum_W_j += W_j
+
+        phi[i][a] = W_i[i][a]/sum_W_j
         
         # Compute the 2 shape functions
-        h[i,0,a] = phi[i,a]
-        h[i,1,a] = phi[i,a] * (X[a] - x[i])/r
+        h[i][0][a] = phi[i][a]
+        h[i][1][a] = phi[i][a] * (X[a] - x[i])/r
+
+# Differentiate the shape functions
+dh_dx = np.zeros((len(x),2,resolution))
+for i in range(N):
+    for j in range(2):
+        for a in range(resolution-1):
+            dh_dx[i][j][a] = (h[i][j][a+1] - h[i][j][a])/(X[a+1]-X[a])
 
 
+# Initialise the stiffness matrix...
+K = np.zeros((N,N,2,2))
+
+# Loop over each node and each degree of freedom...
+for i in range(N):
+    for j in range(N):
+        for m in range(2):
+            for n in range(2):    
+                
+                # Loop from x1 to x2 to numerically integrate...
+                for a in range(0, resolution):
+                    if a == 0 or a==resolution:#(resolution/(N-1)):
+                        f_x = dh_dx[i][m][a] * dh_dx[j][n][a]
+                    else:
+                        f_x = 2*dh_dx[i][m][a] * dh_dx[j][n][a]
+
+                    K[i][j][m][n] += dx/2*f_x
+
+# Force vector...
+f_Im = np.zeros((N, 2))
+fHat_Im = np.zeros((N,2))
+KU_ImJn = np.zeros((N,N,2,2))
+
+for i in range(N):
+    for m in range(2):
+        
+        f_Im[i][m] = np.trapz(X[:]*h[i][m][:], x=None, dx=dx)
+
+        # Evaluate fHat_Im 
+        if i==5: # node is on neumann boundary
+            fHat_Im[i][m] = f_s * h[i][m][resolution-1]
+        elif i==0: # node is on dirichlet boundary
+            sum_KU_ImJn = 0
+            for j in range(N):
+                for n in range(2):
+                    KU_ImJn[i][j][m][n] = -((h[i][m][1]*h[j][n][1] - h[i][m][0]*h[j][n][0]))/(X[1]-X[0])
+                    # sum_KU_ImJn += KU_ImJn
+                    # print(sum_KU_ImJn)
+            
+            fU_Im = u_s*dh_dx[i][m][0]
+
+            fHat_Im[i][m] = sum_KU_ImJn - fU_Im
+        else: 
+            fHat_Im[i][m] = 0
+
+
+f = f_Im + fHat_Im
+print(f)
+# DoF vector...
+
+# q = 
+
+# for i in range(N):
 
 # Plot the shape functions
 ax = plt.axes()
-plt.plot(X,h[0,0,:])
-plt.plot(X,h[0,1,:])
-plt.plot(X,h[1,0,:])
-plt.plot(X,h[1,1,:])
-plt.plot(X,h[2,0,:])
-plt.plot(X,h[2,1,:])
-plt.plot(X,h[3,0,:])
-plt.plot(X,h[3,1,:])
-plt.plot(X,h[4,0,:])
-plt.plot(X,h[4,1,:])
-plt.plot(X,h[5,0,:])
-plt.plot(X,h[5,1,:])
-
+plt.plot(X,h[0][0][:])
+plt.plot(X,h[0][1][:])
+plt.plot(X,h[1][0][:])
+plt.plot(X,h[1][1][:])
+plt.plot(X,h[2][0][:])
+plt.plot(X,h[2][1][:])
+plt.plot(X,h[3][0][:])
+plt.plot(X,h[3][1][:])
+# plt.plot(X,h[5][0][:])
+# plt.plot(X,h[5][0][:])
+# plt.plot(X,h[5][0][:])
 plt.xlabel("x")
 plt.ylabel("Shape functions")
 plt.show()
 
 # Stiffness matrix
-K = np.zeros([N,2,N])
 
 
 # Post-processing...
